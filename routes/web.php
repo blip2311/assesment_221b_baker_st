a570bdbd5b637401f17a67f819462ff865421008
Bipul Haldar <haldar.bipul@gmail.com>
2025-07-23 16:56:11 +0530

    add the required dashboard routes with against the roles

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,6 +9,29 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Auth::routes();
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware(['auth', 'role:Admin'])->group(function () {
+    Route::get('/admin/dashboard', function () {
+        return view('admin.dashboard');
+    })->name('admin.dashboard');
+});
+Route::middleware(['auth', 'role:CRM Agent'])->group(function () {
+    Route::get('/crm/dashboard', function () {
+        return view('crm.dashboard');
+    })->name('crm.dashboard');
+});
+Route::middleware(['auth', 'role:Doctor'])->group(function () {
+    Route::get('/doctor/dashboard', function () {
+        return view('doctor.dashboard');
+    })->name('doctor.dashboard');
+});
+Route::middleware(['auth', 'role:Patient'])->group(function () {
+    Route::get('/patient/dashboard', function () {
+        return view('patient.dashboard');
+    })->name('patient.dashboard');
+});
+Route::middleware(['auth', 'role:Lab Manager'])->group(function () {
+    Route::get('/lab/dashboard', function () {
+        return view('lab.dashboard');
+    })->name('lab.dashboard');
+});
